e4d2198e091c91a2873df1d5b2bc864757499901
old4gy <frustradedman@gmail.com>
2015-10-15 14:14:19 +0300

    first commit

---
+++ javascript/setBackground.js
@@ -0,0 +1,6 @@
+﻿function setBackground(){
+	var background_count = 3;
+	var background_number = Math.floor((Math.random() * background_count) + 1);
+	var bg = "body_class"+background_number;
+	document.getElementById("body_").className=bg;
+}
