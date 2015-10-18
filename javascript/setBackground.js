da8385ac943b744cdc43dd814c6e2585e85a3b2d
old4gy <fknmad@ukr.net>
2015-10-18 18:12:13 +0300

    some css

--- javascript/setBackground.js
+++ javascript/setBackground.js
@@ -3,7 +3,11 @@
 	var background_number = Math.floor((Math.random() * background_count) + 1);
 	var bg = "swamp"+background_number;
 	changeBackground(bg);
+	//loadTimeOut();
 }
 function changeBackground(image){
 	document.getElementsByClassName('body_class0')[0].style.backgroundImage="url(style/images/"+image+".png)";
+}
+function loadTimeOut(){
+	setTimeout ('document.body.style.visibility = \'visible\'', 100);
 }
